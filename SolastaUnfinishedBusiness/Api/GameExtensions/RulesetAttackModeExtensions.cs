370fa85c1a17b1a69ec4fd4ffea02b0511e998e2
EnderWiggin <andrii.gook@gmail.com>
2025-06-29 22:45:13 +0300

    added extensions for RulesetAttackMode

---
+++ SolastaUnfinishedBusiness/Api/GameExtensions/RulesetAttackModeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+
+namespace SolastaUnfinishedBusiness.Api.GameExtensions;
+
+[PublicAPI]
+public static class RulesetAttackModeExtensions
+{
+    internal static RulesetAttackMode Clone(this RulesetAttackMode mode)
+    {
+        var newMode = RulesetAttackMode.AttackModesPool.Get();
+        newMode.Copy(mode);
+
+        return newMode;
+    }
+
+    internal static void Return(this RulesetAttackMode mode)
+    {
+        RulesetAttackMode.AttackModesPool.Return(mode);
+    }
+}
