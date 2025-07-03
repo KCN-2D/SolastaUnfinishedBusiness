2a637e585488b0e91dcea83b5db11273a997cc06
EnderWiggin <andrii.gook@gmail.com>
2025-07-03 18:25:17 +0300

    added `Show Logs` button to `Mod Error Warning` popup

--- SolastaUnfinishedBusiness/Patches/TacticalAdventuresApplicationPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/TacticalAdventuresApplicationPatcher.cs
@@ -1,7 +1,9 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using HarmonyLib;
 using JetBrains.Annotations;
 using SolastaUnfinishedBusiness.Models;
+using UnityEngine;
 using static SolastaUnfinishedBusiness.Models.SaveByLocationContext;
 
 namespace SolastaUnfinishedBusiness.Patches;
@@ -45,6 +47,37 @@ public static class TacticalAdventuresApplicationPatcher
         public static bool Prefix(ref string __result)
         {
             return EnableSaveByLocation(ref __result);
+        }
+    }
+
+    [HarmonyPatch(typeof(TacticalAdventuresApplication), nameof(TacticalAdventuresApplication.HandleLogModException))]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    [UsedImplicitly]
+    public static class HandleLogModException_Patch
+    {
+        [UsedImplicitly]
+        private static bool Prefix(TacticalAdventuresApplication __instance, string logString, string stackTrace,
+            LogType type)
+        {
+            if (type != LogType.Exception || Gui.GuiService == null)
+                return true;
+            Gui.GuiService.ShowMessage(MessageModal.Severity.Serious3, Gui.Localize("Message/&ModErrorWarningTitle"),
+                Gui.Localize("Message/&ModErrorWarningDescription"), "Message/&ModErrorWarningShowLogs", "Screen/&CloseTitle",
+                OpenLogsFolder, null);
+            Application.logMessageReceived -= __instance.HandleLogModException;
+
+
+            return false;
+        }
+
+        private static void OpenLogsFolder()
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = TacticalAdventuresApplication.GameDirectory,
+                UseShellExecute = true,
+                Verb = "open"
+            });
         }
     }
 }
