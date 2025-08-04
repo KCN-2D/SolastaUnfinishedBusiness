1b8e0d5065830737a506ad65a7ab37abfc58033a
EnderWiggin <andrii.gook@gmail.com>
2025-08-04 23:03:00 +0300

    added patch to prevent crash on load if removed from UB power is mentioned in game log

---
+++ SolastaUnfinishedBusiness/Patches/GuiWrapperManagerPatcher.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using SolastaUnfinishedBusiness.Api.ModKit.Utility;
+
+namespace SolastaUnfinishedBusiness.Patches;
+
+[UsedImplicitly]
+public static class GuiWrapperManagerPatcher
+{
+    [HarmonyPatch(typeof(GuiWrapperManager), nameof(GuiWrapperManager.GetGuiPowerDefinition))]
+    [UsedImplicitly]
+    public static class GetGuiPowerDefinition_Patch
+    {
+        [UsedImplicitly]
+        public static bool Prefix(GuiWrapperManager __instance, out GuiPowerDefinition __result, string powerName)
+        {
+            //PATCH: in case we remove Power from UB - if it is referenced in Game Console, it may crash
+            //with this fix it won't crash, but tooltip would look broken
+            __result = __instance.powerDefinitionsMap.GetValueOrDefault(powerName);
+            return false;
+        }
+    }
+}
