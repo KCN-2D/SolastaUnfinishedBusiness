5a783d01ef1d45965e477ec7748e8fb5a029a397
EnderWiggin <andrii.gook@gmail.com>
2025-09-12 11:39:18 +0300

    fixed Bardic Inspiration animation using weapon instead of instrument fixes #15

--- SolastaUnfinishedBusiness/Patches/GraphicsCharacterPatcher.cs
+++ SolastaUnfinishedBusiness/Patches/GraphicsCharacterPatcher.cs
@@ -9,6 +9,52 @@ namespace SolastaUnfinishedBusiness.Patches;
 [UsedImplicitly]
 public static class GraphicsCharacterPatcher
 {
+    private static bool UseInstrumentAnimation(GraphicsCharacter graphics, ActionDefinitions.Id actionId)
+    {
+        if (!graphics.CanUseMusicalInstrumentWhenCasting) { return false; }
+
+        return ActionDefinitions.IsSpellAction(actionId)
+               || actionId is ActionDefinitions.Id.GrantBardicInspiration;
+    }
+
+    [HarmonyPatch(typeof(GraphicsCharacter), nameof(GraphicsCharacter.CastingStart))]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    [UsedImplicitly]
+    public static class CastingStart_Patch
+    {
+        [UsedImplicitly]
+        public static bool Prefix(GraphicsCharacter __instance, ref ActionDefinitions.MagicEffectCastData spellCastData)
+        {
+            //PATCH: fixes Bardic Inspiration animation using weapon instead of instrument
+            if (UseInstrumentAnimation(__instance, spellCastData.ActionId))
+            {
+                __instance.SetWieldedItemsActive(false);
+                __instance.SetWieldedMusicalInstrumentsActive(true);
+            }
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(GraphicsCharacter), nameof(GraphicsCharacter.CastingEnd))]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
+    [UsedImplicitly]
+    public static class CastingEnd_Patch
+    {
+        [UsedImplicitly]
+        public static bool Prefix(GraphicsCharacter __instance, ActionDefinitions.Id actionId)
+        {
+            //PATCH: fixes Bardic Inspiration animation using weapon instead of instrument
+            if (UseInstrumentAnimation(__instance, actionId))
+            {
+                __instance.SetWieldedItemsActive(true);
+                __instance.SetWieldedMusicalInstrumentsActive(false);
+            }
+
+            return false;
+        }
+    }
+
     [HarmonyPatch(typeof(GraphicsCharacter), nameof(GraphicsCharacter.ResetScale))]
     [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Patch")]
     [UsedImplicitly]
